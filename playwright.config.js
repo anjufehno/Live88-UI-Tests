cdc4b9c21ba07e1abb47cc1c1cb25c3bb8fb183c
Jelizaveta Kruglova <114922000+anjufehno@users.noreply.github.com>
2026-09-15 09:42:50 +0300

    Improve Playwright diagnostics configuration

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  reporter: [['html', { open: 'never' }]],
   use: {
     headless: true,
+    trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
